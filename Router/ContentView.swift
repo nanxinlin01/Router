9980a7fa9c44c823f988d8a5155d1fcd0e7cbed7
nanxinlin <nanxinlin@icloud.com>
2026-04-24 14:34:55 +0800

    Initial Commit

---
+++ Router/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Router
+//
+//  Created by 南鑫林 on 2026/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Router/RouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterApp.swift
+//  Router
+//
+//  Created by 南鑫林 on 2026/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
